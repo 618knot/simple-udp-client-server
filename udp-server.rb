fa58fa2ce02d5b4e0b4277e8d425a8c56037319a
618knot <publicac.clt618knot@gmail.com>
2025-01-16 01:18:43 +0900

    arrayをlogにする

--- udp-server.rb
+++ udp-server.rb
@@ -5,10 +5,11 @@ require "logger"
 
 logger = Logger.new("#{Time.now.to_s.gsub(" ", "_")}.log")
 puts "Created Log: #{Time.now.to_s.gsub(" ", "_")}.log"
-puts "UDP Server is Running..."
+puts "UDP Server is Running...\n"
 
 Socket.udp_server_sockets("0.0.0.0", 4096) do |sockets|
   Socket.udp_server_loop_on(sockets) do |msg, _|
-    logger.debug(msg)
+    p msg.bytes
+    logger.debug(msg.bytes)
   end
 end
