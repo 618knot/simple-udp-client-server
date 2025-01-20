c53fb99ee000553a3184e941b77df14429a3ff35
root <publicac.clt618knot@gmail.com>
2025-01-21 00:05:17 +0900

    start flag

--- udp-server.rb
+++ udp-server.rb
@@ -7,10 +7,15 @@ logname = "1-7-1-1_0b"
 logger = Logger.new("../btrlog/#{logname}.log")
 puts "Created Log: ../btrlog/#{logname}.log"
 puts "UDP Server is Running...\n"
-start = Time.now
+start_flg = false
 
 Socket.udp_server_sockets("0.0.0.0", 4096) do |sockets|
   Socket.udp_server_loop_on(sockets) do |msg, _|
+    unless start_flg
+      start = Time.now
+      start_flg = true
+    end
+
     m = msg.bytes
     p m
     logger.debug(m)
