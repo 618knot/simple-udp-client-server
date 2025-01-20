8ac4817859fed6583e63745973dc23302da655e3
root <publicac.clt618knot@gmail.com>
2025-01-20 23:51:34 +0900

    b

--- udp-client.rb
+++ udp-client.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "socket"
 
 udp_soc = UDPSocket.new
@@ -7,10 +5,16 @@ udp_soc = UDPSocket.new
 dst_host = "192.168.11.3"
 dst_port = 4096
 
-msg = [0, 1, 2, 3, 4].pack("C*")
+src_mac = "d8:3a:dd:f6:67:29".upcase.split(":").map { |o| o.to_i(16) }.pack("C*")
+dst_mac = "2C:CF:67:83:EA:E4".split(":").map { |o| o.to_i(16) }.pack("C*")
+# data = "0123456789!#$%&()=-~^|".chars.map(&:ord).pack("C*")
+data = ([0] * 514).pack("C*")
+len = [14 + data.length].pack("S>")
+
+msg = (src_mac + dst_mac + len + data)
 
 loop do
   udp_soc.send(msg, 0, dst_host, dst_port)
-  p "Send: #{msg}"
+  p "Send: #{msg.bytes}"
   sleep(5)
 end
--- udp-server.rb
+++ udp-server.rb
@@ -3,13 +3,19 @@
 require "socket"
 require "logger"
 
-logger = Logger.new("#{Time.now.to_s.gsub(" ", "_")}.log")
-puts "Created Log: #{Time.now.to_s.gsub(" ", "_")}.log"
+logname = "1-7-1-1_0b"
+logger = Logger.new("../btrlog/#{logname}.log")
+puts "Created Log: ../btrlog/#{logname}.log"
 puts "UDP Server is Running...\n"
+start = Time.now
 
 Socket.udp_server_sockets("0.0.0.0", 4096) do |sockets|
   Socket.udp_server_loop_on(sockets) do |msg, _|
-    p msg.bytes
-    logger.debug(msg.bytes)
+    m = msg.bytes
+    p m
+    logger.debug(m)
+    if m.last == 255
+      logger.debug("TIME: #{Time.now - start}")
+    end
   end
 end
