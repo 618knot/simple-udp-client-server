081523102553f11681641af1cf77320a38feb54f
root <publicac.clt618knot@gmail.com>
2025-01-23 15:07:03 +0900

    3

--- udp-client.rb
+++ udp-client.rb
@@ -10,7 +10,7 @@ dst_port = 4096
 cnt = 0
 
 src_mac = [0, 0, 0, 0, 0, 0].pack("C*")
-dst_mac = "2C:CF:67:83:EA:E4".split(":").map { |o| o.to_i(16) }.pack("C*")
+dst_mac = "FF:FF:FF:FF:FF:FF".split(":").map { |o| o.to_i(16) }.pack("C*")
 data = [cnt] * 514
 len = [14 + data.length].pack("S>")
 
@@ -20,7 +20,7 @@ loop do
   msg = src_mac + dst_mac + len + ([cnt] * 514).pack("C*")
   udp_soc.send(msg, 0, dst_host, dst_port)
   p "##{cnt} Send: #{msg.bytes}"
-  return if cnt >= 255
-  sleep(1)
-  cnt += 1
+  # return if cnt >= 255
+  sleep(5)
+  # cnt += 1
 end
