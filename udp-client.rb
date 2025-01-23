3aa83e135e93ad0a0bd43ea568f4b6c5e5b68911
root <publicac.clt618knot@gmail.com>
2025-01-23 11:58:29 +0900

    b

--- udp-client.rb
+++ udp-client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "socket"
 
 udp_soc = UDPSocket.new
@@ -5,16 +7,20 @@ udp_soc = UDPSocket.new
 dst_host = "192.168.11.3"
 dst_port = 4096
 
-src_mac = "d8:3a:dd:f6:67:29".upcase.split(":").map { |o| o.to_i(16) }.pack("C*")
+cnt = 0
+
+src_mac = [0, 0, 0, 0, 0, 0].pack("C*")
 dst_mac = "2C:CF:67:83:EA:E4".split(":").map { |o| o.to_i(16) }.pack("C*")
-# data = "0123456789!#$%&()=-~^|".chars.map(&:ord).pack("C*")
-data = ([0] * 514).pack("C*")
+data = [cnt] * 514
 len = [14 + data.length].pack("S>")
 
-msg = (src_mac + dst_mac + len + data)
+ 
 
 loop do
+  msg = src_mac + dst_mac + len + ([cnt] * 514).pack("C*")
   udp_soc.send(msg, 0, dst_host, dst_port)
-  p "Send: #{msg.bytes}"
-  sleep(5)
+  p "##{cnt} Send: #{msg.bytes}"
+  return if cnt >= 255
+  sleep(1)
+  cnt += 1
 end
