08b777c52f3866e4e38a88aecb420a5032e10492
root <publicac.clt618knot@gmail.com>
2025-01-23 10:50:37 +0900

    client

--- udp-client.rb
+++ udp-client.rb
@@ -7,10 +7,18 @@ udp_soc = UDPSocket.new
 dst_host = "192.168.11.3"
 dst_port = 4096
 
-msg = [0, 1, 2, 3, 4].pack("C*")
+cnt = 0
+
+src_mac = [0, 0, 0, 0, 0, 0].pack("C*")
+dst_mac = "2C:CF:67:83:EA:E4".split(":").map { |o| o.to_i(16) }.pack("C*")
+data = [5] * 514
+len = [14 + data.length].pack("S>")
+
+msg = src_mac + dst_mac + len + data.pack("C*")
 
 loop do
   udp_soc.send(msg, 0, dst_host, dst_port)
-  p "Send: #{msg}"
-  sleep(5)
+  p "##{cnt} Send: #{msg.bytes}"
+  sleep(0.001)
+  cnt += 1
 end
