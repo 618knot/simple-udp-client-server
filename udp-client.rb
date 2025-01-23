acc4d0957c4018428331ab0a9dea5a179a95ad6e
618knot <publicac.clt618knot@gmail.com>
2025-01-23 17:45:28 +0900

    seg

--- udp-client.rb
+++ udp-client.rb
@@ -4,23 +4,28 @@ require "socket"
 
 udp_soc = UDPSocket.new
 
-dst_host = "192.168.11.3"
+dst_host = "192.168.11.2"
 dst_port = 4096
 
 cnt = 0
 
 src_mac = [0, 0, 0, 0, 0, 0].pack("C*")
-dst_mac = "FF:FF:FF:FF:FF:FF".split(":").map { |o| o.to_i(16) }.pack("C*")
-data = [cnt] * 514
-len = [14 + data.length].pack("S>")
+mac_addrs = [
+  "2C:CF:67:76:5A:16",
+  "2C:CF:67:81:66:E3",
+  "2C:CF:67:83:ED:56"
+]
+mac_addrs.map! { |mac| mac.split(":").map { |o| o.to_i(16) }.pack("C*") }
+len = [14 + 514].pack("S>")
 
  
 
 loop do
-  msg = src_mac + dst_mac + len + ([cnt] * 514).pack("C*")
-  udp_soc.send(msg, 0, dst_host, dst_port)
-  p "##{cnt} Send: #{msg.bytes}"
-  # return if cnt >= 255
+  mac_addrs.each_with_index do |mac, i|
+    msg = src_mac + mac + len + ([i] * 514).pack("C*")
+    udp_soc.send(msg, 0, dst_host, dst_port)
+    p "##{cnt} Send: #{msg.bytes}"
+  end
   sleep(5)
-  # cnt += 1
+  cnt += 1
 end
