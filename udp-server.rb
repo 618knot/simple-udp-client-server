459bb00d1ff6ae31ff033cabf602149db8e54ff5
618knot <publicac.clt618knot@gmail.com>
2025-01-24 13:17:11 +0900

    b

--- udp-server.rb
+++ udp-server.rb
@@ -3,11 +3,12 @@
 require "socket"
 require "logger"
 
-logname = "1-7-1-1_0b"
+logname = "1-7-2-0_1b2"
 logger = Logger.new("../btrlog/#{logname}.log")
 puts "Created Log: ../btrlog/#{logname}.log"
 puts "UDP Server is Running...\n"
 start_flg = false
+strat = nil
 
 Socket.udp_server_sockets("0.0.0.0", 4096) do |sockets|
   Socket.udp_server_loop_on(sockets) do |msg, _|
